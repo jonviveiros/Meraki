c285945ad28ca15894037b61f4cb9e22ff53bc79
jonviveiros <jon.viveiros@gmail.com>
2020-12-11 08:22:35 -0500

    commit mx config script

---
+++ mx_config_info.py
@@ -0,0 +1,143 @@
+# !/usr/bin/env python3
+# https://github.com/jonviveiros/Meraki/ms_switchport_config_info.py
+
+# DESCRIPTION
+# The goal is to pull configuration information from security appliances in a Meraki
+# network and then export to an Excel file. Use printOrgs.py to get net_id
+
+# USAGE
+# python mx_config_info.py -k <api_key> -o <org_id> -n <net_id>
+# arguments -k and -o are required, argument -n is optional
+
+import csv
+from datetime import datetime
+import getopt
+import sys
+import requests
+import json
+import meraki
+
+# Prints READ_ME help message for user to read
+def print_help():
+    print('This python script requires arguments.')
+    print('     -h   Help')
+    print('     -k   API Key')
+    print('     -0   Org ID')
+    print('     -n   Net ID (optional)')
+
+
+# Oops, something bad happened
+def print_error():
+    print('You made a fatal error.')
+    print('Try again.')
+
+
+def get_inventory(api_key, org_id):
+    url = 'https://api.meraki.com/api/v0/organizations/{}/inventory'.format(org_id)
+    try:
+        response = requests.get(url=url, headers={'X-Cisco-Meraki-API-Key': api_key, 'Content-Type': 'application/json'})
+        return json.loads(response.text)
+    except requests.exceptions.RequestException as e:
+        print('Error calling get_inventory: {}'.format(e))
+
+
+def get_switchports(api_key, serial):
+    url = 'https://api.meraki.com/api/v0/devices/{}/switchPorts'.format(serial)
+    try:
+        response = requests.get(url=url, headers={'X-Cisco-Meraki-API-Key': api_key, 'Content-Type': 'application/json'})
+        return json.loads(response.text)
+    except requests.exceptions.RequestException as e:
+        print('Error calling get_switchports: {}'.format(e))
+
+
+def main(argv):
+    # Set default values for command line arguments
+    api_key = None
+    org_id = None
+    net_id = None
+
+
+    # Get command line arguments
+    try:
+        opts, args = getopt.getopt(argv, 'hk:o:n:')
+    except getopt.GetoptError:
+        print_help()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print_help()
+            sys.exit()
+        elif opt == '-k':
+            api_key = arg
+            print('Your API Key is: '+api_key)
+        elif opt == '-o':
+            org_id = arg
+            print('Your Org ID is: '+org_id)
+        elif opt == '-n':
+            net_id = arg
+            print('Your Net ID is: '+net_id)
+    # TODO net_id is optional - may need to be removed in future
+
+    # Check if all required parameters have been input
+    if api_key == None or org_id == None:
+        print_help()
+        sys.exit(2)
+
+    # Set the CSV output file and write the header row
+    timenow = '{:%Y%m%d_%H%M%S}'.format(datetime.now())
+    filename = 'ms_switchport_config_{0}.csv'.format(timenow)
+    output_file = open(filename, mode='w', newline='\n')
+    field_names = ['Hostname', 'Serial Number', 'Port', 'Description', 'Mode', 'VLAN', 'VoiceVLAN', 'Allowed VLANS']
+    csv_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    csv_writer.writerow(field_names)
+    print('STATUS: Created file: '+str(filename))
+
+    # Find all MS switches and save to a 'switches' dictionary
+    session = requests.session()
+    inventory = get_inventory(api_key, org_id)
+    #switches = [device for device in inventory if device['model'][:2] in ('MS') and device['networkId'] is not None]
+    MXs = [device for device in inventory if device['model'][:2] in ('MX') and device['networkId'] is not None]
+    print(MXs)
+    # For loop to cycle through each MX
+    # ----Creates a list of serial numbers and hostnames
+    MX_host_serials = []
+    net_count = []
+    for MX in MXs:
+        if MX['serial'] not in MX_host_serials:
+            MX_host_serials.append([MX['serial'],MX['name']])
+
+    # ----Creates a list of network IDs; used only for status counter today
+        if MX['networkId'] not in net_count:
+            net_count.append(MX['networkId'])
+
+    # Prints out a multi-level lists with serial numbers and hostnames
+    print (MX_host_serials)
+    # Prints total # of switches in organization
+    print('Found a total of %d MXs configured across %d networks in this organization.' % (len(MXs), len(net_count)))
+
+    i = 0
+    # For loop to iterate through each rows in switch_host_serials
+    for row in range(len(MX_host_serials)):
+
+        # Prints a quick status counter, serial number and hostname
+        print('Count '+str(i+1)+'/'+str(len(MXs))+':  Gathering data for '+MX_host_serials[i][1]+' - '+MX_host_serials[i][0])
+
+        # Creates a dictionary of switchport data, csv_row looks for each item's key and places the value
+#        switchports = get_switchports(api_key, switch_host_serials[row][0])
+#        for item in switchports:
+#            csv_row = [switch_host_serials[i][1]], [switch_host_serials[i][0]], item['number'], item['name'], item['type'], item['vlan'], item['voiceVlan'], item['allowedVlans']
+#            csv_writer.writerow(csv_row)
+            #break
+        i += 1
+        #break
+
+
+if __name__ == '__main__':
+    inputs = sys.argv[1:]
+    try:
+        key_index = inputs.index ('-k')
+    except ValueError:
+        print_help()
+        sys.exit(2)
+
+    main(sys.argv[1:])
